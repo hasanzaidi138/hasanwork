5d45a1f4b9a7d414a918dc4a319ba12d161e9ec2
hasanzaidi138 <82930507+hasanzaidi138@users.noreply.github.com>
2021-08-13 12:55:22 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-import cv2
+import sqlite3
 
-face
+conn= sqlite3.connect("new_db")
+
+print("Enter the Employeeid")
+r=input()
+print("Enter the employee city")
+ct=input()
+print("Enter employee name")
+nm= input()
+
+query= "insert into employee_data(employeeid,employeecity,employeename) values (" + r + ",'" + nm + "','" + ct +"')"
+
+conn.execute(query)
+conn.commit()
+conn.close()
+
+print("Data Saved")
+
